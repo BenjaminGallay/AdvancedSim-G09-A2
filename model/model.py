57bfabf9ae37db6fe3ca8a871b2d4b765e7b982c
Groumphh <groumphh38@gmail.com>
2026-03-01 17:05:10 +0100

    truncated N1 road to match Chittagong-Dhaka road

--- model/model.py
+++ model/model.py
@@ -87,7 +87,7 @@ class BangladeshModel(Model):
         BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
         # Input paths
-        df = pd.read_csv(os.path.join(BASE_DIR, "data", "roads.csv"))
+        df = pd.read_csv(os.path.join(BASE_DIR, "data", "roadN1_trunc.csv"))
 
         # a list of names of roads to be generated
         roads = ["N1"]
